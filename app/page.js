726404cc37a45e7978e2df99e96d3316e9f64980
Sr_ePrint_Onlinwork <sronline99890@gmail.com>
2026-09-09 06:53:57 +0530

    Change background color to lightOrange

--- app/page.js
+++ app/page.js
@@ -559,7 +559,7 @@ export default function Home() {
             borderRadius: '12px',
             outline: 'none',
             marginBottom: '15px',
-            background: 'white',
+            background: 'lightOrange',
           }}
         />
 
