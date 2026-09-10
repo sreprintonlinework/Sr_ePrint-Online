7b7026e1a654d2dca2dfabb92031084bb0255377
Sr_ePrint_Onlinwork <sronline99890@gmail.com>
2026-09-10 17:50:04 +0530

    Fix import path for pdfs in create-order route

--- app/create-order/route.js
+++ app/create-order/route.js
@@ -1,5 +1,5 @@
 import { NextResponse } from 'next/server';
-import { pdfs } from '../../pdfs';
+import { pdfs } from '../pdfs';
 
 export const dynamic = 'force-dynamic';
 export const runtime = 'nodejs';
