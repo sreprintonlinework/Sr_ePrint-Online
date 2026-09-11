751d3798b45983a6362962a9a685a0b6d2c2bbe5
Sr_ePrint_Onlinwork <sronline99890@gmail.com>
2026-09-11 20:11:06 +0530

    Update PDF details including names and prices

--- app/pdfs.js
+++ app/pdfs.js
@@ -1,27 +1,27 @@
 export const pdfs = [
   {
     id: 'raj-001',
-    name: 'birth-appli',
-    file: 'birth-appli.pdf',
-    price: 99,
+    name: 'birth-application',
+    file: 'birth-application.pdf',
+    price: 49,
   },
   {
     id: 'raj-002',
-    name: 'sample1',
-    file: 'sample1.pdf',
-    price: 20,
+    name: 'kids-abc-word-tracing',
+    file: 'kids-abc-word-tracing.pdf',
+    price: 10,
   },
   {
     id: 'raj-003',
-    name: 'newdoc',
-    file: 'newdoc.pdf',
+    name: 'pan-appli-form93',
+    file: 'pan-appli-form93.pdf',
     price: 99,
   },
   {
     id: 'raj-004',
-    name: 'tsmsidc',
-    file: 'tsmsidc.pdf',
-    price: 20,
+    name: 'Surendar Leave Form 11',
+    file: 'Surendar Leave Form 11.xls',
+    price: 59,
   },
     {
     id: 'raj-005',
