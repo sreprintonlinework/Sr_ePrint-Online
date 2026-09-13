88f1ae9c16e7193185a2e77df761f810bb8a6515
Sr_ePrint_Onlinwork <sronline99890@gmail.com>
2026-09-14 01:00:09 +0530

    Fix formatting and update names in pdfs.js

--- app/pdfs.js
+++ app/pdfs.js
@@ -1,32 +1,36 @@
 export const pdfs = [
   {
     id: 'raj-001',
-    name: 'Birth Application Form - Sample Template Guide(For Reference Only',
+    name: 'Birth Application Form - Sample Template Guide (For Reference Only)',
     file: 'birth-application.pdf',
     price: 20,
   },
+
   {
     id: 'raj-002',
     name: 'kids-ABC-word-tracing',
     file: 'kids-abc-word-tracing.pdf',
     price: 2,
   },
+
   {
     id: 'raj-003',
-    name: 'Pan-application-form 93 - Sample Template Guide(For Reference Only',
-   ) file: 'pan-appli-form93.pdf',
+    name: 'Pan-application-form 93 - Sample Template Guide (For Reference Only)',
+    file: 'pan-appli-form93.pdf',
     price: 99,
   },
+
   {
     id: 'raj-004',
+    name: 'Surendar Leave Form 11 - Sample Template Guide (For Reference Only)',
+    file: 'Surendar Leave Form 11.xls',
+    price: 29,
+  },
+
+  {
+    id: 'raj-005',
     name: 'TSPSC-gen-studies-Modelpaper',
     file: 'tspsc-gen-studies-modelpaper.pdf',
     price: 59,
-  },
-    {
-    id: 'raj-005',
-    name: 'Surendar Leave Form 11 - Sample Template Guide(For Reference Only',
-    file: 'Surendar Leave Form 11.xls',
-    price: 29,
   },
 ];
