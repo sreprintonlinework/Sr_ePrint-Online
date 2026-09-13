015e71437bd70f145a693526d3381634370532b1
Sr_ePrint_Onlinwork <sronline99890@gmail.com>
2026-09-14 01:58:24 +0530

    Refactor business address in page.js
    Updated the business address formatting for clarity.

--- app/page.js
+++ app/page.js
@@ -1183,10 +1183,10 @@ export default function Home() {
 
             Phone / WhatsApp: 9989057683, Email: sronline99890@gmail.com
             <br />
-            Business Address: Sr internet online center,
-            New Maa Mart backside, Ieeja,
-            Jogulamba Gadwal Dist, Telangana-509127, 
-            India.
+            Business Address: 
+          <br />
+            
+          Sr internet online center,Ieeja, JogulambaGadwal Dist, Telangana-509127, India.
 
             <br />
           </p>
