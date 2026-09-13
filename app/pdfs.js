46c4811b841e4e7f41850d175b8954b65197d1dc
Sr_ePrint_Onlinwork <sronline99890@gmail.com>
2026-09-14 00:51:02 +0530

    Update template names to indicate reference only

--- app/pdfs.js
+++ app/pdfs.js
@@ -1,7 +1,7 @@
 export const pdfs = [
   {
     id: 'raj-001',
-    name: 'Birth Application Form - Sample Template Guide',
+    name: 'Birth Application Form - Sample Template Guide(For Reference Only',
     file: 'birth-application.pdf',
     price: 20,
   },
@@ -13,7 +13,7 @@ export const pdfs = [
   },
   {
     id: 'raj-003',
-    name: 'Pan-application-form 93 -Sample Template Guide',
+    name: 'Pan-application-form 93 - Sample Template Guide(For Reference Only',
    ) file: 'pan-appli-form93.pdf',
     price: 99,
   },
@@ -25,7 +25,7 @@ export const pdfs = [
   },
     {
     id: 'raj-005',
-    name: 'Surendar Leave Form 11 - Sample Template Guide',
+    name: 'Surendar Leave Form 11 - Sample Template Guide(For Reference Only',
     file: 'Surendar Leave Form 11.xls',
     price: 29,
   },
